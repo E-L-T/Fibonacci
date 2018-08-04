2d896a0e35aa3989fe2931c23b402e6fa82e36e7
Eric Leroy-Terquem <ericleroyterquem@yahoo.fr>
2018-08-04 23:56:04 +0200

    continue test environment configuration, can record a user in database

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe 'User', type: :feature do
+  scenario 'create an user' do
+    visit sign_up_path
+    expect do
+      fill_in :user_email, with: 'email@test.com'
+      fill_in :user_password, with: 'password_operateur'
+      click_on 'Sign up'
+    end.to change { User.count }.by(+1)
+  end
+end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,7 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
+require 'capybara/poltergeist'
 # Add additional requires below this line. Rails is not loaded until this point!
+Capybara.javascript_driver = :poltergeist
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
@@ -29,6 +31,7 @@ ActiveRecord::Migration.maintain_test_schema!
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.include Capybara::DSL, type: :feature
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
