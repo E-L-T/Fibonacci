5a1ce9af8d6b39f82ac0821d9ca08b21b40532d9
Eric Leroy-Terquem <ericleroyterquem@yahoo.fr>
2018-08-08 23:35:55 +0200

    allow user to get back to the correct form step after browser closing

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class DashboardController < ApplicationController
   def show
+    if user.present? && user.state != 'completed'
+      redirect_to edit_user_path user.id
+      flash[:info] = "You can continue to fill the form where you stopped"
+    end
+  end
+
+  def user
+    User.where(remember_token: request.headers['cookie'].split('remember_token=')[1])[0]
   end
 end
