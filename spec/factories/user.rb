7489d00604d7b6a081ad9bb9b8c653a9316e4863
Eric Leroy-Terquem <ericleroyterquem@yahoo.fr>
2018-08-12 00:08:10 +0200

    rewrite tests

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,7 +1,16 @@
+
 FactoryBot.define do
     factory :user do
       email 'test@mail.com'
       password 'password'
-      pdl 'Point de livraison principal'
+      state 'uncompleted'
+      pdl 'Point 2'
+      first_name 'John'
+      last_name 'Richard'
+      street_number '2'
+      street_name 'grande rue'
+      zip_code '78110'
+      city 'Chatou'
+      situation 1
     end
   end
--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 describe 'User', type: :feature do
+  let(:half_completed_user) { create :user, state: 'half_completed' }
 
-  scenario 'create a user with a go back' do
+  scenario 'create a user' do
     visit sign_up_path
     expect do
-      fill_in :user_email, with: 'henri@andre.com'
+      fill_in :user_email, with: 'john@bryan.com'
       fill_in :user_password, with: '1234!'
-      fill_in :user_first_name, with: 'Henri'
-      fill_in :user_last_name, with: 'André'
+      fill_in :user_first_name, with: 'John'
+      fill_in :user_last_name, with: 'Bryan'
       fill_in :user_street_number, with: '1 ter'
       fill_in :user_street_name, with: 'Grande rue'
       fill_in :user_zip_code, with: '60210'
@@ -19,16 +20,30 @@ describe 'User', type: :feature do
     expect(current_path).to eq edit_user_path User.last
     expect(User.last).to have_state(:half_completed)
     expect(User.last).to transition_from(:half_completed).to(:completed).on_event(:complete)
-    click_on 'Go back'
-    fill_in :user_email, with: 'email@test.fr'
-    fill_in :user_password, with: 'password_secret'
-    click_on 'Go to next step'
 
     fill_in :user_pdl, with: 'Point central'
     choose :user_situation_move_in
     click_on 'Submit'
+
     expect(User.last).to have_state(:completed)
-    expect(User.last.email).to eq 'email@test.fr'
-    expect(current_path).to eq '/'
+    expect(User.last.email).to eq 'john@bryan.com'
+    expect(current_path).to eq root_path
+  end
+
+  scenario 'can go back when filling the form' do
+    visit edit_user_path half_completed_user
+    expect(page).to have_content 'Technical informations'
+    click_on 'Go back'
+    expect(User.last).to have_state(:uncompleted)
+    fill_in :user_email, with: 'john@gmail.com'
+    fill_in :user_password, with: '12345!'
+    click_on 'Go to next step'
+    expect(User.last).to have_state(:half_completed)
+    fill_in :user_pdl, with: 'Point alternatif'
+    choose :user_situation_move_in
+    click_on 'Submit'
+    expect(User.last).to have_state(:completed)
+    expect(User.last.email).to eq 'john@gmail.com'
+    expect(current_path).to eq root_path
   end
 end
